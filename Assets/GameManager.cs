3b3810193477ce5fbf603c2f99377eb3d0199946
Zachary Rock <zacharyrock@hotmail.com>
2014-01-06 14:14:03 -0800

    Game Manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject Teemo;
+	public GameObject Malphite;
+
+	List<GameObject> availableObjects;
+	List<GameObject> usedObjects;
+
+	void Start () {
+		availableObjects = new List<GameObject>();
+		usedObjects = new List<GameObject>();
+	}
+
+	void Update () {
+		
+	}
+
+	void SpawnTeemo() {
+
+	}
+
+	void SpawnMalphite() {
+
+	}
+}
